899b98c989f2b2b2c1f7ccd4a544a29572fa527e
Casey Gruppioni <cgruppioni@gmail.com>
2017-05-17 16:27:59 -0400

    Comment out tmux

--- tag-tmux/zsh/before/tmux.zsh
+++ tag-tmux/zsh/before/tmux.zsh
@@ -1,8 +1,8 @@
-inside_ssh(){
-  [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
-}
+# inside_ssh(){
+#   [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
+# }
 
-if ! inside_ssh; then
-  source "$BASE/tmux.zsh"
-  ensure_we_are_inside_tmux
-fi
+# if ! inside_ssh; then
+#   source "$BASE/tmux.zsh"
+#   ensure_we_are_inside_tmux
+# fi
